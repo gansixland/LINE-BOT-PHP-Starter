cc77661ef09f474cadfb8941a42bffdd59c87f6c
gansixland <gansixland@users.noreply.github.com>
2017-06-09 14:41:13 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -11,15 +11,15 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			         if($event['message']['text'] == "สวัสดี"){
-  $messages = ['type' => 'text','text' => "สวัสดี"];
-}else if($event['message']['text']== "ชื่ออะไร"){
+			         if($event['message']['text'] == 'สวัสดี'){
+  $messages = ['type' => 'text','text' => 'สวัสดี'];
+}else if($event['message']['text']== 'ชื่ออะไร'){
 
-    $messages = ['type' => 'text','text' => "ฉันยังไม่มีชื่อนะ"];
-}else if($event['message']['text'] == "ทำอะไรได้บ้าง"){
-   $messages = ['type' => 'text','text' => "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ"]; 
+    $messages = ['type' => 'text','text' => 'ฉันยังไม่มีชื่อนะ'];
+}else if($event['message']['text'] == 'ทำอะไรได้บ้าง'){
+   $messages = ['type' => 'text','text' => 'ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ']; 
 }else{
-   $messages = ['type' => 'text','text' => "ฉันไม่เข้าใจ"]; 
+   $messages = ['type' => 'text','text' => 'ฉันไม่เข้าใจ']; 
 }
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
