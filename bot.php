80f272d343e5f504f302af5bf8b43e9ad4e36b0b
gansixland <gansixland@users.noreply.github.com>
2017-06-09 14:55:12 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = ['type' => 'text','text' => $text];
+			$messages = ['type' => 'text','text' => 'Test'];
 
 
     
