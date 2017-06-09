442fea8a7d41f39a49d02c3706095e2300e221db
gansixland <gansixland@users.noreply.github.com>
2017-06-09 14:44:05 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -11,16 +11,10 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			         if($event['message']['text'] == 'สวัสดี'){
-  $messages = ['type' => 'text','text' => 'สวัสดี'];
-}else if($event['message']['text']== 'ชื่ออะไร'){
-
-    $messages = ['type' => 'text','text' => 'ฉันยังไม่มีชื่อนะ'];
-}else if($event['message']['text'] == 'ทำอะไรได้บ้าง'){
-   $messages = ['type' => 'text','text' => 'ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ']; 
-}else{
-   $messages = ['type' => 'text','text' => 'ฉันไม่เข้าใจ']; 
-}
+			                  if($event['message']['text'] == 'สวัสดี'){
+                $messages = ['type' => 'text','text' => $text];
+}else {}
+    
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
